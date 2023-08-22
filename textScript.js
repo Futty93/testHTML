988253f5c45c53dca7717d72cdf81c953a9157ee
Futty93 <kazuki.futawatari.s5@dc.tohoku.ac.jp>
2023-08-22 19:13:09 +0900

    Add files via upload

--- textScript.js
+++ textScript.js
@@ -1,23 +1,30 @@
 const textbox = document.getElementById('textbox');
 const copyBtn = document.getElementById('copy');
 
-textbox.addEventListener('input', function() {
-let value = textbox.value;
+textbox.addEventListener('input', function () {
+    let value = textbox.value;
 
-// sanitize value
-value = value.replace(/[^a-zA-Z0-9\s]/g, '');
+    // sanitize value
+    value = value.replace(/[^a-zA-Z0-9\s]/g, '');
 
-console.log(value);
+    console.log(value);
 });
 
-copyBtn.addEventListener('click', function() {
-if(!textbox.value) {
-alert("Please enter text");
-return;
-}
+copyBtn.addEventListener('click', function () {
+    if (!textbox.value) {
+        alert("Please enter text");
+        return;
+    }
 
-navigator.clipboard.writeText(textbox.value)
-.then(() => {
-alert("Copied to clipboard!");
-});
+    navigator.clipboard.writeText(textbox.value)
+        .then(() => {
+            const alert = document.createElement('div');
+            alert.textContent = 'Copied to clipboard!';
+            alert.style.position = 'absolute';
+            document.body.appendChild(alert);
+
+            setTimeout(() => {
+                alert.remove();
+            }, 1000);
+        });
 });
